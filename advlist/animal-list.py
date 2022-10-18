1a6de4af0b2438e9d58403e788763d2e6b60e6c2
GaiusOctopus <GaiusOctopus@users.noreply.github.com>
2022-10-18 17:19:01 +0000

    Learning about dictionaries

---
+++ advlist/animal-list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+### TLG NDE Python | LAncheta | Animal List Challenge
+
+def main():
+
+    animal_list= ["Fox", "Fly", "Ant", "Bee", "Cod", "Cat", "Dog", "Yak", "Cow", "Hen", "Koi", "Hog", "Jay", "Kit"]
+
+main()
