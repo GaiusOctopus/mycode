e0581408552295c22c7f9f2ac67b674aae657c58
GaiusOctopus <GaiusOctopus@users.noreply.github.com>
2022-10-19 17:11:59 +0000

    Playing with python pip packages

---
+++ if_name/first_module.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" TLG NDE Python | LAncheta """
+
+print("This code will always execute.")
+
+def main():
+
+    print("This code belongs to the main function in Module 1.")
+
+if __name__ == "__main__":
+    main()
---
+++ if_name/second_module.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" TLG NDE Python | LAncheta """
+
+import first_module
+
+first_module.main()
+
+print("Module #2 Name=", __name__)
